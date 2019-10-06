a6f037a249a55a5c14f8eb8d98ec734aed6ca7fa
Pornpimol-p <pornpimol_p@kkumail.com>
2019-10-06 20:08:06 +0700

    remove if

--- src/LAB9/FizzBuzz.java
+++ src/LAB9/FizzBuzz.java
@@ -4,9 +4,6 @@ public class FizzBuzz {
 	Scanner keyboardScanner = new Scanner(System.in);
 	
 	public String getNumber1(int number1) {
-		if(number1 % 3 == 0) {
-			 return Fizz();
-		 }
 		return Integer.toString(number1);
 	}
 
