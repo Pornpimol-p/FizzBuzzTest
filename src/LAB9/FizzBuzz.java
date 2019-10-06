4f5aedfd359583ee729bd06e60a71bd5f6d19061
Pornpimol-p <pornpimol_p@kkumail.com>
2019-10-06 20:05:47 +0700

    up if num=3 is Fizz

--- src/LAB9/FizzBuzz.java
+++ src/LAB9/FizzBuzz.java
@@ -4,7 +4,10 @@ public class FizzBuzz {
 	Scanner keyboardScanner = new Scanner(System.in);
 	
 	public String getNumber1(int number1) {
-		 return Integer.toString(number1);
+		if(number1 % 3 == 0) {
+			 return Fizz();
+		 }
+		return Integer.toString(number1);
 	}
 
 }
